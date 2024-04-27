0efc870322b466bed09aaf194918bafa291bc254
Ramiru <ramiruthehan@gmail.com>
2024-04-27 23:20:18 +0530

    initial commit

---
+++ js/quiz.js
@@ -0,0 +1,227 @@
+const quizArr = [
+  {
+    id: 1,
+    question: "What does ICT stand for?",
+    options: {
+      A: "International Computer Technology",
+      B: "Information and Communication Technology",
+      C: "Integrated Communication Tool",
+      D: "Internet Computing Techniques",
+    },
+    answer: "B",
+  },
+  {
+    id: 2,
+    question: "Which of the following is an example of hardware?",
+    options: {
+      A: "Microsoft Word",
+      B: "Google Chrome",
+      C: "Keyboard",
+      D: "Adobe Photoshop",
+    },
+    answer: "C",
+  },
+  {
+    id: 3,
+    question: "Which of the following is not a programming language?",
+    options: {
+      A: "JPEG",
+      B: "Python",
+      C: "C++",
+      D: "Java",
+    },
+    answer: "A",
+  },
+  {
+    id: 4,
+    question: "What is the purpose of a router in a network?",
+    options: {
+      A: "To store data",
+      B: "To display web pages",
+      C: "To connect multiple networks",
+      D: "To create animations",
+    },
+    answer: "C",
+  },
+  {
+    id: 5,
+    question: "Which protocol is used for sending emails?",
+    options: {
+      A: "FTP",
+      B: "HTTP",
+      C: "SMTP",
+      D: "TCP/IP",
+    },
+    answer: "C",
+  },
+  {
+    id: 6,
+    question:
+      "What does HTML stand for?",
+    options: {
+      A: "Hyper Text Markup Language",
+      B: "High Text Markup Language",
+      C: "Hyperlink Transfer Markup Language",
+      D: "Hyper Text Media Language",
+    },
+    answer: "A",
+  },
+  {
+    id: 7,
+    question: "What is the primary function of a firewall?",
+    options: {
+      A: "To enhance the speed of the internet connection",
+      B: "To store and manage large amounts of data",
+      C: "To prevent unauthorized access to or from a private network",
+      D: "To create web pages",
+    },
+    answer: "C",
+  },
+  {
+    id: 8,
+    question: "Which of the following is an example of cloud computing service?",
+    options: {
+      A: "Microsoft Office 365",
+      B: "Adobe Photoshop",
+      C: "Windows Operating System",
+      D: "Google Chrome",
+    },
+    answer: "A",
+  },
+  {
+    id: 9,
+    question:
+      "What is the purpose of an operating system?",
+    options: {
+      A: "To manage hardware resources and provide a user interface",
+      B: "To create documents and presentations",
+      C: "To send and receive emails",
+      D: "To design websites",
+    },
+    answer: "A",
+  },
+  {
+    id: 10,
+    question: "Which of the following is an example of a mobile operating system?",
+    options: {
+      A: "Windows",
+      B: "iOS",
+      C: "Linux",
+      D: "macOS",
+    },
+    answer: "B",
+  },
+  {
+    id: 11,
+    question: "What does VPN stand for?",
+    options: {
+      A: "Virtual Private Network",
+      B: "Virtual Public Network",
+      C: "Visual Private Network",
+      D: "Visual Public Network",
+    },
+    answer: "A",
+  },
+  {
+    id: 12,
+    question: "What is the function of a modem?",
+    options: {
+      A: "To convert digital signals to analog signals and vice versa",
+      B: "To display web pages",
+      C: "To store data",
+      D: "To manage network traffic",
+    },
+    answer: "A",
+  },
+  {
+    id: 13,
+    question: "Which of the following is an example of a spreadsheet software?",
+    options: {
+      A: "Microsoft Excel",
+      B: "Microsoft Word",
+      C: "Adobe Photoshop",
+      D: "Google Chrome",
+    },
+    answer: "A",
+  },
+  {
+    id: 14,
+    question: "What is the purpose of a database management system (DBMS)?",
+    options: {
+      A: "To manage and organize data",
+      B: "To create websites",
+      C: "To send emails",
+      D: "To edit images",
+    },
+    answer: "A",
+  },
+  {
+    id: 15,
+    question: "Which of the following is a common method of data storage?",
+    options: {
+      A: "Typewriter",
+      B: "Floppy Disk",
+      C: "VHS Tape",
+      D: "Vinyl Record",
+    },
+    answer: "B",
+  },
+  {
+    id: 16,
+    question:
+      "What is the function of a CPU (Central Processing Unit)?",
+    options: {
+      A: "To store data permanently",
+      B: "To display images on a screen",
+      C: "To process instructions and perform calculations",
+      D: "To connect to the internet",
+    },
+    answer: "C",
+  },
+  {
+    id: 17,
+    question: "Which of the following is an example of a computer network?",
+    options: {
+      A: "The Internet",
+      B: "A single computer",
+      C: "A printer",
+      D: "A keyboard",
+    },
+    answer: "A",
+  },
+  {
+    id: 18,
+    question: "What is the purpose of a browser?",
+    options: {
+      A: "To edit images",
+      B: "To connect to the internet and access web pages",
+      C: "To manage network traffic",
+      D: "To store and organize data",
+    },
+    answer: "B",
+  },
+  {
+    id: 19,
+    question:
+      "What is the significance of TCP/IP in computer networking?",
+    options: {
+      A: "It is a programming language",
+      B: "It is a hardware component",
+      C: "It is a type of operating system",
+      D: "It is a set of protocols that governs how data is transmitted over the internet",
+    },
+    answer: "D",
+  },
+  {
+    id: 20,
+    question:
+      "Which of the following is not a type of computer virus?",
+    options: {
+      A: "Trojan Horse",
+      B: "Worm",
+      C: "Ransomware",
+      D: "Firewall",
+    },
+    answer: "D",
+  },
+];
